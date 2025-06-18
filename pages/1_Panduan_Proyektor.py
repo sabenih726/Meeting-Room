929d15b686b38357b40c93156518c2c1b34a03a6
sabenih726 <sabenih726@gmail.com>
2025-06-18 15:04:53 +0700

    Update 1_Panduan_Proyektor.py

--- pages/1_Panduan_Proyektor.py
+++ pages/1_Panduan_Proyektor.py
@@ -4,7 +4,7 @@ st.title("📺 Panduan Menyambungkan Proyektor")
 st.markdown("#### A. Menggunakan Shortcut Keyboard")
 st.write("1. Pastikan proyektor menyala (tombol power di remote).")
 st.write("2. Tekan tombol `Windows + K` pada laptop Anda.")
-st.image("assets/shortcut-windows-k.png", caption="Shortcut Windows + K")
+st.image("assets/windows_k_shortcut.png", caption="Shortcut Windows + K")
 st.write("3. Pilih nama ruang meeting yang muncul.")
 
 st.markdown("---")
